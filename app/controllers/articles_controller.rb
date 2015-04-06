550060826e98c26822ebdd9884e6d5930a9feea6
ted0101 <f50715@yahoo.com.tw>
2015-04-06 14:46:00 +0800

    finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,23 @@ class ArticlesController < ApplicationController
 			render:new	
 		end
 	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to root_path
+		else
+			render:edit
+		end
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to root_path
+		
+	end
 
 
 
